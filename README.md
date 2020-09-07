263fd9c435c26225bc019d26352305e79f3d3173
canozo <canwoahzo@gmail.com>
2020-09-07 15:06:11 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ASP.NET Core with React starter
+
+Starter web project for a web application with ASP.NET Core and React.
---
+++ ReactStarter.Web/Client/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>
+          Bienvenido a mi prueba de React Typescript con ASP.NET Core
+        </p>
+        <a
+          className="app-link"
+          href="https://github.com/canozo/acklen-app-testing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Repositorio
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
