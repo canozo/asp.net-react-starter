3c42855641fc905e65d32d207d340e57f503ab89
canozo <canwoahzo@gmail.com>
2020-09-15 18:12:14 -0600

    Add various npm packages to React app

--- NetWorthCalc.Web/Client/src/App.tsx
+++ NetWorthCalc.Web/Client/src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="app">
-      <header className="app-header">
-        <p>
-          ASP.NET Core + React Typescript
-        </p>
-        <a
-          className="app-link"
-          href="https://github.com/canozo/asp.net-react-starter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Repo
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={() => <div>Landing</div>} />
+      <Route path="/app" component={() => <div>App</div>} />
+      <Route path="/logout" component={() => <div>Logout</div>} />
+      <Route component={() => <div>Not Found</div>} />
+    </Switch>
   );
 }
 
