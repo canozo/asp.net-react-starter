04c9df72260323f54f0b1d840dd7f4023d5dde80
canozo <canwoahzo@gmail.com>
2020-09-16 22:01:03 -0600

    Modify App.test.tsx

--- NetWorthCalc.Web/Client/src/App.test.tsx
+++ NetWorthCalc.Web/Client/src/App.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders repository link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/repo/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders landing page just fine', () => {
+  const { getAllByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  );
+  const elements = getAllByText(/net worth/i);
+  expect(elements.length).toBeGreaterThan(0);
+});
+
+test('routing works when not found', () => {
+  const { getAllByText } = render(
+    <MemoryRouter initialEntries={['/unknown']}>
+      <App />
+    </MemoryRouter>
+  );
+  const elements = getAllByText(/not found/i);
+  expect(elements.length).toBeGreaterThan(0);
 });
