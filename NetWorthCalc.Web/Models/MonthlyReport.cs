ee26e527200c2381a85a110ccbb4c50ebadc9a81
canozo <canwoahzo@gmail.com>
2020-09-17 02:42:06 -0600

    Make 'UserId', 'Month' and 'Year' private in MonthlyReport

--- NetWorthCalc.Web/Models/MonthlyReport.cs
+++ NetWorthCalc.Web/Models/MonthlyReport.cs
@@ -5,21 +5,24 @@ namespace NetWorthCalc.Web.Models
 {
     public class MonthlyReport
     {
-        public MonthlyReport()
+        public MonthlyReport(string UserId, int Month, int Year)
         {
             MonthlyReportId = new Guid();
             CreatedOn = DateTime.Now;
+            this.UserId = UserId;
+            this.Month = Month;
+            this.Year = Year;
         }
 
         public Guid MonthlyReportId { get; private set; }
 
         public DateTime CreatedOn { get; private set; }
 
-        public string UserId { get; set; }
+        public string UserId { get; private set; }
 
-        public int Month { get; set; }
+        public int Month { get; private set; }
 
-        public int Year { get; set; }
+        public int Year { get; private set; }
 
         public List<Asset> Assets { get; set; }
 
