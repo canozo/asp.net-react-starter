336030d6f8bae67d71a76556a8c23dd59e00b757
canozo <canwoahzo@gmail.com>
2020-09-18 17:25:13 -0600

    Add react-chartjs-2 go show Net Worth history

--- NetWorthCalc.Web/Client/src/pages/History/index.tsx
+++ NetWorthCalc.Web/Client/src/pages/History/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Line } from 'react-chartjs-2';
 import Body from '../../components/Body';
 import './History.scss';
 
 const History: React.FC = () => {
+  const [data, setData] = useState({});
+
   return (
     <Body
       title="Net worth history"
       subtitle="See how your net worth has changed over the last year."
     >
-      History
+      <Line data={data} />
     </Body>
   );
 };
