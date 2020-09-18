ffa1e5850aeaebe51e55c7f0514ef0f5165a90df
canozo <canwoahzo@gmail.com>
2020-09-18 17:50:11 -0600

    Little modifications to the chart

--- NetWorthCalc.Web/Client/src/pages/History/index.tsx
+++ NetWorthCalc.Web/Client/src/pages/History/index.tsx
@@ -3,8 +3,42 @@ import { Line } from 'react-chartjs-2';
 import Body from '../../components/Body';
 import './History.scss';
 
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const cyan = 'rgba(75,192,192,1)';
+const cyanTransparent = 'rgba(75,192,192,0.4)';
+const grey = 'rgba(220,220,220,1)';
+
+const dataset = {
+  lineTension: 0.1,
+  backgroundColor: cyanTransparent,
+  borderColor: cyan,
+  pointBorderColor: cyan,
+  pointBackgroundColor: 'white',
+  pointHoverRadius: 5,
+  pointHoverBackgroundColor: cyan,
+  pointHoverBorderColor: grey,
+  pointHoverBorderWidth: 2,
+
+  label: 'Year 2020',
+  data: [65, -10, 80, 81, 56, 55, 40]
+};
+
 const History: React.FC = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState({ labels, datasets: [dataset] });
 
   return (
     <Body
