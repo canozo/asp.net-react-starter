b2eb71d51f9c551713bdf99bf1dc2d3724676146
canozo <canwoahzo@gmail.com>
2020-09-18 15:40:16 -0600

    Remove assets button added

--- NetWorthCalc.Web/Client/src/pages/Info/index.tsx
+++ NetWorthCalc.Web/Client/src/pages/Info/index.tsx
@@ -102,6 +102,52 @@ const Info: React.FC = () => {
     }
   };
 
+  const changeAssetName = async (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    const newReport: MonthlyReport | null = clone(monthlyReport);
+    if (newReport === null) {
+      return;
+    }
+    newReport.assets[index].name = event.target.value;
+    setMonthlyReport(newReport);
+  };
+
+  const changeAssetAmount = async (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    const number = Number(event.target.value);
+    if (number < 0) {
+      return;
+    }
+
+    const newReport: MonthlyReport | null = clone(monthlyReport);
+    if (newReport === null) {
+      return;
+    }
+    newReport.assets[index].amount = number;
+    setMonthlyReport(newReport);
+  };
+
+  const deleteAsset = async (asset: Asset) => {
+    try {
+      const token = await getAccessTokenSilently();
+      const response = await fetch(`/api/asset/${asset.assetId}`, {
+        method: 'delete',
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+      });
+
+      if (response.ok) {
+        const newReport: MonthlyReport | null = clone(monthlyReport);
+        if (newReport === null) {
+          return;
+        }
+        newReport.assets = newReport.assets.filter(a => a.assetId !== asset.assetId);
+        setMonthlyReport(newReport);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   if (monthlyReport == null) {
     return (
       <Body
@@ -117,7 +163,6 @@ const Info: React.FC = () => {
     );
   }
 
-
   return (
     <Body
       title="Monthly report information"
@@ -131,17 +176,52 @@ const Info: React.FC = () => {
           <div className="col-lg mt-3">
             {/* Assets */}
             <p className="form-label">Assets</p>
-            <button type="button" className="btn btn-primary" onClick={addAsset}>
+            <button type="button" className="btn btn-primary mb-3" onClick={addAsset}>
               Add new asset
             </button>
-            {monthlyReport.assets.map(asset => (
-              <div key={asset.assetId}>{asset.assetId}</div>
+            {monthlyReport.assets.map((asset, index) => (
+              <div key={asset.assetId} className="mb-5">
+                <div className="mb-1">
+                  <label htmlFor={`assetName#${index}`} className="form-label">
+                    Asset name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id={`assetName#${index}`}
+                    value={asset.name}
+                    onChange={event => changeAssetName(event, index)}
+                  />
+                </div>
+                <div className="mb-1">
+                  <label htmlFor={`assetAmount#${index}`} className="form-label">
+                    Asset amount
+                  </label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id={`assetAmount#${index}`}
+                    value={asset.amount}
+                    onChange={event => changeAssetAmount(event, index)}
+                  />
+                </div>
+                <div className="mb-1">
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={() => deleteAsset(asset)}
+                  >
+                    Delete asset
+                  </button>
+                </div>
+              </div>
             ))}
           </div>
+
           <div className="col-lg mt-3">
             {/* Liabilities */}
             <p className="form-label">Liabilities</p>
-            <button type="button" className="btn btn-primary" onClick={addLiability}>
+            <button type="button" className="btn btn-primary mb-3" onClick={addLiability}>
               Add new liability
             </button>
             {monthlyReport.liabilities.map(liability => (
