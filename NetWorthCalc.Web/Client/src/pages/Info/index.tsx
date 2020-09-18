6399bdff664fba8cd0f748f7f4cd2b23c7397d38
canozo <canwoahzo@gmail.com>
2020-09-18 14:48:34 -0600

    Verify if ID is valid

--- NetWorthCalc.Web/Client/src/pages/Info/index.tsx
+++ NetWorthCalc.Web/Client/src/pages/Info/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import MonthlyReport from '../../interfaces/MonthlyReport';
 import Body from '../../components/Body';
@@ -9,6 +9,7 @@ const Info: React.FC = () => {
   const [monthlyReport, setMonthlyReport] = useState<MonthlyReport | null>(null);
   const { getAccessTokenSilently } = useAuth0();
   const location = useLocation();
+  const history = useHistory();
 
   const path = location.pathname.split('/');
   const monthlyReportId = path[path.length - 1];
@@ -17,15 +18,20 @@ const Info: React.FC = () => {
     const getData = async () => {
       try {
         const token = await getAccessTokenSilently();
-        const response = fetch(`/api/monthlyreport/${monthlyReportId}`, {
+        const response = await fetch(`/api/monthlyreport/${monthlyReportId}`, {
           method: 'get',
           headers: {
             Authorization: `Bearer ${token}`
           },
         });
-      const result: MonthlyReport = await response.then(res => res.json());
-      setMonthlyReport(result);
 
+        if (!response.ok) {
+          history.push('/app');
+          return;
+        }
+
+        const result: MonthlyReport = await response.json();
+        setMonthlyReport(result);
       } catch (error) {
         console.log(error);
       }
@@ -33,11 +39,9 @@ const Info: React.FC = () => {
     getData();
   }, []);
 
-  console.log(monthlyReport);
-
   return (
     <Body
-      title="Report information"
+      title="Monthly report information"
       subtitle="Modify your report informacion."
     >
       Info
