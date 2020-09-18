4a50f9727169404445693bf93ab699894a23c3d1
canozo <canwoahzo@gmail.com>
2020-09-18 00:44:29 -0600

    Start the dashboard view

---
+++ NetWorthCalc.Web/Client/src/components/SideMenu/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SideMenu.scss';
+
+const SideMenu: React.FC = () => {
+  return (
+    <div id="side-menu">
+      Hey
+    </div>
+  );
+};
+
+export default SideMenu;
--- NetWorthCalc.Web/Client/src/pages/Dashboard/index.tsx
+++ NetWorthCalc.Web/Client/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import SideMenu from '../../components/SideMenu';
 
 const Dashboard: React.FC = () => {
   const { getAccessTokenSilently } = useAuth0();
@@ -10,8 +11,11 @@ const Dashboard: React.FC = () => {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div>
-      Dashboard
+    <div className="d-flex justify-content-between min-vh-100">
+      <SideMenu />
+      <div className="container-fluid bg-dark">
+        <div className="container bg-primary">2</div>
+      </div>
     </div>
   );
 };
