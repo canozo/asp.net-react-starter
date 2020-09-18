1d7ac1994fb40ed3df578a059d64619615a5d220
canozo <canwoahzo@gmail.com>
2020-09-18 12:04:35 -0600

    Side menu and verify if user is logged when entering dashboard

--- NetWorthCalc.Web/Client/src/components/SideMenu/index.tsx
+++ NetWorthCalc.Web/Client/src/components/SideMenu/index.tsx
@@ -1,13 +1,73 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Power, ArrowLeft, ArrowRight, Briefcase, PlusSquare, Calendar } from 'react-feather';
 import './SideMenu.scss';
 
 const SideMenu: React.FC = () => {
   const [shrink, setShrink] = useState(false);
+  const { logout, user } = useAuth0();
+  const history = useHistory();
+
+  console.log(user);
+
+  let classNames = 'd-flex flex-column justify-content-around align-items-center';
+  if (shrink) {
+    classNames += ' shrink';
+  }
 
   return (
-    <div id="side-menu" className={shrink ? 'shrink' : ''}>
-      <button type="button" onClick={() => setShrink(!shrink)}>
-        {shrink ? '>' : '<'}
+    <div id="side-menu" className={classNames}>
+      {/* Toggle shrink */}
+      <button
+        type="button"
+        className="btn btn-light btn-icon"
+        onClick={() => setShrink(!shrink)}
+      >
+        {shrink ? <ArrowRight size={18} /> : <ArrowLeft size={18} />}
+      </button>
+
+      {/* Options and avatar */}
+      <div className="options d-flex flex-column justify-content-evenly align-items-center">
+        <div className="option text-center" onClick={() => history.push('/app/reports')}>
+          <button
+            type="button"
+            className="btn btn-light btn-icon"
+            onClick={() => history.push('/app/reports')}
+          >
+            <Briefcase />
+          </button>
+          {shrink ? null : <p className="opt-text pt-2 css-fade">Monthly Reports</p>}
+        </div>
+        <div className="option text-center" onClick={() => history.push('/app/new')}>
+          <button
+            type="button"
+            className="btn btn-light btn-icon"
+            onClick={() => history.push('/app/new')}
+          >
+            <PlusSquare />
+          </button>
+          {shrink ? null : <p className="opt-text pt-2 css-fade">New Monthly Report</p>}
+        </div>
+        <div className="option text-center" onClick={() => history.push('/app/history')}>
+          <button
+            type="button"
+            className="btn btn-light btn-icon"
+            onClick={() => history.push('/app/history')}
+          >
+            <Calendar />
+          </button>
+          {shrink ? null : <p className="opt-text pt-2 css-fade">History</p>}
+        </div>
+      </div>
+
+      {/* Exit */}
+      <button
+        type="button"
+        className="btn btn-light btn-icon"
+        onClick={() => logout({ returnTo: window.location.origin })}
+      >
+        <Power size={18} />
       </button>
     </div>
   );
--- NetWorthCalc.Web/Client/src/pages/Dashboard/index.tsx
+++ NetWorthCalc.Web/Client/src/pages/Dashboard/index.tsx
@@ -1,14 +1,30 @@
 import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import SideMenu from '../../components/SideMenu';
 
 const Dashboard: React.FC = () => {
-  const { getAccessTokenSilently } = useAuth0();
+  const { isLoading, isAuthenticated } = useAuth0();
+  const history = useHistory();
 
   useEffect(() => {
-    getAccessTokenSilently()
-      .catch(err => console.log(err));
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    if (isLoading) {
+      return;
+    }
+    if (!isAuthenticated) {
+      history.push('/');
+    }
+  }, [history, isLoading, isAuthenticated]);
+
+  if (isLoading) {
+    return (
+      <div className="d-flex min-vh-100 align-items-center justify-content-center">
+        <div className="spinner-border text-dark" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="d-flex justify-content-between min-vh-100">
