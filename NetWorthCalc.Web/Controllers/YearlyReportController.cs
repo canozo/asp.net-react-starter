a66d1fa2dfaf1c7e0d39bd9f552ec5338fcf01a0
canozo <canwoahzo@gmail.com>
2020-09-18 20:15:55 -0600

    Add endpoint to get latest networht status from the user

--- NetWorthCalc.Web/Controllers/YearlyReportController.cs
+++ NetWorthCalc.Web/Controllers/YearlyReportController.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using NetWorthCalc.Web.Models;
 using Microsoft.EntityFrameworkCore.Internal;
 
@@ -24,7 +22,69 @@ namespace NetWorthCalc.Web.Controllers
             _context = context;
         }
 
-        [HttpGet("{year}")]
+        [HttpGet("latest")]
+        public IActionResult GetNetWorth()
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var queryAssets = from report in _context.Set<MonthlyReport>()
+                              join asset in _context.Set<Asset>()
+                                  on report.MonthlyReportId equals asset.MonthlyReportId into grouping
+                              from asset in grouping.DefaultIfEmpty()
+                              where report.UserId == userId
+                              group asset by new
+                              {
+                                  Key = asset.MonthlyReportId,
+                                  report.FullDate
+                              } into assetGroup
+                              orderby assetGroup.Key.FullDate descending
+                              select new NetWorthReportItem
+                              {
+                                  MonthlyReportId = assetGroup.Key.Key,
+                                  FullDate = assetGroup.Key.FullDate,
+                                  Total = assetGroup.Sum(a => a.Amount)
+                              };
+
+            var queryLiabilities = from report in _context.Set<MonthlyReport>()
+                                   join liability in _context.Set<Liability>()
+                                       on report.MonthlyReportId equals liability.MonthlyReportId into grouping
+                                   from liability in grouping.DefaultIfEmpty()
+                                   where report.UserId == userId
+                                   group liability by new
+                                   {
+                                       Key = liability.MonthlyReportId,
+                                       report.FullDate
+                                   } into liabilityGroup
+                                   orderby liabilityGroup.Key.FullDate descending
+                                   select new NetWorthReportItem
+                                   {
+                                       MonthlyReportId = liabilityGroup.Key.Key,
+                                       FullDate = liabilityGroup.Key.FullDate,
+                                       Total = liabilityGroup.Sum(a => a.Amount)
+                                   };
+
+            var latestAsset = queryAssets.FirstOrDefault();
+            var latestLiability = queryLiabilities.FirstOrDefault();
+
+            var result = new NetWorthReportItem()
+            {
+                MonthlyReportId = latestAsset.MonthlyReportId ?? latestLiability.MonthlyReportId,
+                FullDate = latestAsset.FullDate ?? latestLiability.FullDate,
+                Total = latestAsset.Total - latestLiability.Total,
+            };
+
+            return Ok(result);
+        }
+
+        public class NetWorthReportItem
+        {
+            public Guid? MonthlyReportId { get; set; }
+
+            public DateTime? FullDate { get; set; }
+
+            public double Total { get; set; }
+        }
+
+        [HttpGet("report/{year}")]
         public IEnumerable GetReport(int year)
         {
             string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
@@ -53,12 +113,12 @@ namespace NetWorthCalc.Web.Controllers
                                    {
                                        Key = liability.MonthlyReportId,
                                        report.Month
-                                   } into assetGroup
+                                   } into liabilityGroup
                                    select new YearlyReportItem
                                    {
-                                       MonthlyReportId = assetGroup.Key.Key,
-                                       Month = assetGroup.Key.Month,
-                                       Total = assetGroup.Sum(a => a.Amount)
+                                       MonthlyReportId = liabilityGroup.Key.Key,
+                                       Month = liabilityGroup.Key.Month,
+                                       Total = liabilityGroup.Sum(a => a.Amount)
                                    };
 
             List<YearlyReportItem> result = new List<YearlyReportItem>();
